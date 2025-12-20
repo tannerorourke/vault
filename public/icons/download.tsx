388c41d257489053a25847d6f2876a3127a6b9ea
Tanner O'Rourke <tannero@live.com>
2025-12-20 04:47:02 -0800

    replace raw svg's with exported functions

---
+++ public/icons/download.tsx
@@ -0,0 +1,14 @@
+export function DownloadIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="48"
+      height="48"
+      viewBox="0 0 24 24"
+      {...props}
+      fill="currentColor"
+    >
+      <path fill="currentColor" d="M5 20h14v-2H5m14-9h-4V3H9v6H5l7 7z" />
+    </svg>
+  );
+}
