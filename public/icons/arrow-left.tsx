c308e15e7e27291b5db81375a9702b540d436f19
Tanner O'Rourke <tannero@live.com>
2025-12-21 02:27:14 -0800

    add functional icons and home images

--- public/icons/arrow-left.tsx
+++ public/icons/arrow-left.tsx
@@ -1,4 +1,4 @@
-export function ArrowLeftIcon(props: React.SVGProps<SVGSVGElement>) {
+export function ArrowLeftIcon(props?: React.SVGProps<SVGSVGElement>) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
--- public/icons/window.tsx
+++ public/icons/window.tsx
@@ -7,8 +7,8 @@ export function WindowIcon(props: React.SVGProps<SVGSVGElement>) {
       {...props}
     >
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M1.5 2.5h13v10a1 1 0 0 1-1 1h-11a1 1 0 0 1-1-1zM0 1h16v11.5a2.5 2.5 0 0 1-2.5 2.5h-11A2.5 2.5 0 0 1 0 12.5zm3.75 4.5a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5M7 4.75a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0m1.75.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5"
         fill="currentColor"
       />
