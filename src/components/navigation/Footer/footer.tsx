91c00977979bcf6f5a8c0411d00d18a86e83ec2c
Tanner O'Rourke <tannero@live.com>
2026-05-20 14:34:35 -0700

    footer on about page only

---
+++ src/components/navigation/Footer/footer.tsx
@@ -0,0 +1,44 @@
+import Sheet from "@/components/ui/Sheet";
+import Text from "@/components/ui/Text";
+import TextLink from "@/components/ui/TextLink";
+import { FOOTER, REPO_LINK } from "@/content/nav-links";
+import * as sty from "./footer.css";
+import { ComponentProps } from "react";
+
+
+export type FooterProps = ComponentProps<'div'> & {
+  withDivider?: boolean
+};
+
+export function Footer({ className, withDivider, ...rest }: FooterProps) {
+  return (
+    <div className={className} {...rest}>
+      {withDivider && 
+        <hr className={sty.divider} aria-hidden="true" />
+      }
+      <Sheet className={sty.footerSheet}>
+        <Text
+          as="span"
+          variant="bodySm"
+          tone="secondary"
+          className={sty.footerLabel}
+        >
+          {FOOTER}
+        </Text>
+        <TextLink
+          label={'View Source'}
+          rightArrow={{ hold: true, dir: "right" }}
+          underline="always"
+          textProps={{ variant: "bodySm" }}
+          nextProps={{
+            href: REPO_LINK.href || "#",
+            target: REPO_LINK.target,
+            rel: REPO_LINK.target === "_blank" ? "noopener noreferrer" : undefined,
+            className: sty.footerLink
+          }}
+          aria-label={REPO_LINK.alt}
+        />
+      </Sheet>
+    </div>
+  )
+}
